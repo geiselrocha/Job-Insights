8829aaa12fc1d72a6213c937b097d1bd3ac0f085
geiselrocha <geiselrocha@hotmail.com>
2023-01-25 22:40:28 -0300

    feat: implements function to filter by job type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -63,4 +63,8 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    results = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            results.append(job)
+    return results
