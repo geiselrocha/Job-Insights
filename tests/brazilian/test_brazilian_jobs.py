9a4caf50291afc2e820e084b0233b1ae05604250
geiselrocha <geiselrocha@hotmail.com>
2023-01-31 20:41:57 -0300

    fix: test_brazilian_jobs function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,6 +2,5 @@ from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    # pass
     result = {"title": "Motorista", "salary": "3000", "type": "full time"}
     assert read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0] == result
